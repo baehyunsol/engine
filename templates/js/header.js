09b3ecfe21ef9b98022cd014da3683a8ee9428a3
baehyunsol <47506131+baehyunsol@users.noreply.github.com>
2023-01-03 16:07:53 +0900

    animate header

--- templates/js/header.js
+++ templates/js/header.js
@@ -3,6 +3,15 @@ let topmenu = document.querySelector("div.topmenu");
 function toggle_header() {
     topmenu.classList.toggle("activated");
     topmenu.classList.toggle("deactivated");
+
+    if (topmenu.style.maxHeight) {
+        topmenu.style.maxHeight = null;
+    }
+
+    else {
+        topmenu.style.maxHeight = topmenu.scrollHeight + "px";
+    }
+
 }
 
 document.getElementById("navbutton").addEventListener("click", toggle_header);
